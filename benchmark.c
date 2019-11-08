d344799091d90c9121909d1a04dcdd073d50de04
TysonRayJones <tyson.jones.input@gmail.com>
2019-11-08 16:59:08 +0000

    calc qubits from available memory

--- benchmark.c
+++ benchmark.c
@@ -8,12 +8,14 @@
 #include <sys/time.h>
 #include "QuEST.h"
 
+
 void circTest(Qureg qureg) {
     int numQb = qureg.numQubitsRepresented;
     
     initPlusState(qureg);
     
-    for (int reps=0; reps<10; reps++) {
+    // some arbitrary circuit requiring invoking all modes of caching
+    for (int rep=0; rep<10; rep++) {
         for (int i=0; i<numQb; i++)
             hadamard(qureg, i);
         for (int i=0; i<numQb; i++)
@@ -23,81 +25,191 @@ void circTest(Qureg qureg) {
     }
 }
 
+
 void memTest(Qureg qureg) {
+    int numQb = qureg.numQubitsRepresented;
     
     initPlusState(qureg);
     
-    for (int reps=0; reps<20; reps++) {
+    // just copying data back and forth between RAM and VRAM
+    // (arbitrarily modifying to spook off compilers)
+    for (int rep=0; rep<20; rep++) {
         copyStateFromGPU(qureg);
         
         // multiply by i (phase fac of PI)
-        qreal im = qureg.stateVec.imag[reps];
-        qureg.stateVec.imag[reps] = qureg.stateVec.real[reps];
-        qureg.stateVec.real[reps] = - im;
+        int ind = rep % qureg.numAmpsTotal;
+        qreal im = qureg.stateVec.imag[ind];
+        qureg.stateVec.imag[ind] = qureg.stateVec.real[ind];
+        qureg.stateVec.real[ind] = - im;
         
         copyStateToGPU(qureg);
-        hadamard(qureg, reps%qureg.numQubitsRepresented);
+        sqrtSwapGate(qureg, rep%numQb, (rep+1)%numQb);
     }
 }
 
-int main (int narg, char *varg[]) {
 
-    if (narg != 3) {
-        printf(
-            "Insufficient args! Run as:\n\n"
-            "./benchmark TESTCODE NUMQUBITS\n\n"
-            "where\n\n"
-            "TESTCODE is 0 or 1:\n\t"
-                "0 to run and time an arbitrary circuit\n\t"
-                "1 to time direct state-vector modification\n\n"
-            "NUMQUBITS should satisfy (in order to ~fill RAM):\n\t"
-                "CPU/GPU: 16 * 2^NUMQUBITS bytes < RAM\n\t"
-                "distributed: 2 * 16 * 2^NUMQUBITS bytes < RAM\n\n"
-            "The duration (in seconds) of the test is written to file:\n\t"
-            "'benchmark_results_test[TESTCODE]_[NUMQUBITS]qubits.txt'\n"
-        );
-        exit(1);
-    }
-    
-    // setup
-    int testCode = atoi(varg[1]);
-    int numQb = atoi(varg[2]);
-    QuESTEnv env = createQuESTEnv();
-    Qureg qureg = createQureg(numQb, env);
-    
+long double timeFuncCall(void (*func)(Qureg), Qureg arg) {
+
     // start timing
     struct timeval timeInst;
     gettimeofday(&timeInst, NULL);
     long double startTime = (
 		timeInst.tv_sec + (long double) timeInst.tv_usec/pow(10,6));
-    
-    // perform test
-    if (testCode == 0)
-        circTest(qureg);
-    else
-        memTest(qureg);
         
+    func(arg);
+    
     // stop timing
     gettimeofday(&timeInst, NULL);
     long double endTime = (
         timeInst.tv_sec + (long double) timeInst.tv_usec/pow(10,6));
-    long double duration = endTime - startTime;
-    printf("duration: %.10Lf\n", duration);
         
-    // prepare filename 
-    char fn[500];
-    sprintf(fn, "benchmark_results_test%d_%dqubits.txt", testCode, numQb);
-    FILE *fp = fopen(fn, "w");
-    if (fp) {
-        fprintf(fp, "%.10Lf\n", duration);
-        printf("written to: %s\n", fn);
-        fclose(fp);
-    } else {
-        printf("FAILED to write to %s\n", fn);
+    return endTime - startTime;
+}
+
+
+void timeRepeatFuncCalls(
+    void (*func)(Qureg), Qureg arg, int numReps, 
+    long double *avDur, long double *varDur, int rank
+) {    
+    long double sumDur = 0;
+    long double sumSquaredDur = 0;
+    
+    // disable line-buffering to indicate progress
+    setbuf(stdout, NULL);
+    
+    // repeatedly time the test
+    for (int i=0; i<numReps; i++) {
+        
+        // indicate progress
+        for (int percent=0; percent<=100; percent+=10)
+            if (i == floor(percent * numReps / 100))
+                if (rank == 0)
+                    printf("%d%% ", percent);
+                    
+        // perform test
+        long double dur = timeFuncCall(func, arg);
+        sumDur += dur;
+        sumSquaredDur += dur*dur;
+    }
+        
+    // E[dur] = sum(dur)/numReps
+    *avDur = sumDur/numReps;
+    
+    // Var[dur] = E[dur^2] - E[dur]^2 = sum(dur^2)/N - E[dur]^2
+    *varDur = (sumSquaredDur/numReps) - (*avDur)*(*avDur);
+}
+
+void assert(int cond, char* msg, int rank) {
+    if (!cond) {
+        if (rank == 0)
+            printf("ERROR: %s\nExiting...\n", msg);
+        exit(1);
+    }
+}
+
+
+int main (int narg, char *varg[]) {
+    
+    // setup immediately to get rank info
+    QuESTEnv env = createQuESTEnv();
+    
+    if (narg != 7) {
+        
+        if (env.rank == 0)
+            printf(
+                "Insufficient args! Run as:\n\n"
+                "./benchmark NUM_NODES NUM_THREADS IS_GPU TEST_TYPE MEM_SIZE NUM_SAMPLES\n\n"
+                "where\n\n"
+                "NUM_NODES is the number of employed nodes (1 if not distributed).\n\t"
+                    "Must be a power of 2.\n\n"
+                "NUM_THREADS is the number of employed threads.\n\t"
+                    "If >1, QuEST must be compiled in multithreaded mode (using OpenMP).\n\n"
+                "IS_GPU is 0 or 1 to indicate whether benchmark is GPU-accelerated.\n\t"
+                    "if 1, then necessarily NUM_NODES=1 and NUM_THREADS=1.\n\n"
+                "TEST_TYPE is 0 or 1 to indicate which test to perform.\n\t"
+                    "0: an arbitrary circuit operating on all qubits\n\t"
+                    "1: copying of memory back and forth between RAM and VRAM\n\t"
+                    "   (latter should only be used if IS_GPU=1)\n\n"
+                "MEM_SIZE is the total per-node (or per-GPU) memory available, in GiB.\n\t"
+                    "if IS_GPU=0, this is the total RAM per node (e.g. 64)\n\t"
+                    "if IS_GPU=1, this is the total VRAM of the GPU (e.g. 4)\n\t"
+                    "Testing will be performed with as big a quantum state-vector as can fit, "
+                    "calculated via:\n\t\t"
+                        "NUM_QUBITS = Floor[26 + Log2[MEM_SIZE - .5]]\n\t"
+                    "which assumes double-precision and an overhead of ~500 MiB.\n"
+                    "Note distributed tests will simulate 1 fewer qubit, due to comm overheads.\n\n"
+                "NUM_SAMPLES is the number of repetitions of the test (informing the mean and variance).\n\n"
+                "The average duration and variance of the test is written to file (by node rank 0):\n\t"
+                "'results_[NUM_NODES]n_[NUM_THREADS]t_[IS_GPU]g_[TEST_TYPE]test_[NUM_QUBITS]q.txt'\n"
+                "(where NUM_QUBITS is derived from MEM_SIZE and NUM_NODES) along with "
+                "copies of these arguments.\n\n");
+        exit(1);
+    }
+    
+    // get cmd args
+    int i=1;
+    int NUM_NODES   = atoi(varg[i++]);
+    int NUM_THREADS = atoi(varg[i++]);
+    int IS_GPU      = atoi(varg[i++]);
+    int TEST_TYPE   = atoi(varg[i++]);
+    int MEM_SIZE    = atoi(varg[i++]);
+    int NUM_SAMPLES = atoi(varg[i++]);
+    
+    // validate args 
+    assert((NUM_NODES&(NUM_NODES-1))==0, "NUM_NODES must be an exponent of 2.", env.rank);
+    if (IS_GPU)
+        assert(NUM_NODES==1 && NUM_THREADS==1, "IS_GPU=1 requires NUM_NODES=NUM_THREADS=1.", env.rank);
+    if (TEST_TYPE == 1)
+        assert(IS_GPU, "TEST_TYPE=1 requires IS_GPU=1.", env.rank);
+    assert(MEM_SIZE > 1, "MEM_SIZE must be at least 1 GiB.", env.rank);
+        
+    int NUM_QUBITS = floor(26 + log2(MEM_SIZE - .5)) + ((NUM_NODES>1)? (-1):0);
+    
+    // report args
+    char argsBuff[1000];
+    sprintf(argsBuff, 
+        "NUM_NODES: %d\nNUM_THREADS: %d\nIS_GPU: %d\n"
+        "TEST_TYPE: %d\nMEM_SIZE: %d\nNUM_QUBITS: %d\nNUM_SAMPLES: %d\n",
+        NUM_NODES, NUM_THREADS, IS_GPU, TEST_TYPE, MEM_SIZE, NUM_QUBITS, NUM_SAMPLES);
+        
+    if (env.rank == 0)
+        printf("\n%s\nSimulating...\n", argsBuff);
+    
+    // setup QuEST
+    Qureg qureg = createQureg(NUM_QUBITS, env);
+    void (*testFunc)(Qureg) = (TEST_TYPE==1)? memTest : circTest;
+    
+    // perform benchmark
+    long double avDur, varDur;
+    timeRepeatFuncCalls(testFunc, qureg, 10, &avDur, &varDur, env.rank);
+    
+    if (env.rank == 0)
+        printf("\nDone!\nDuration: %Lg (mean) %Lg (variance)\n\n", avDur, varDur);
+    
+    // create output filename
+    char fnBuff[500];
+    sprintf(fnBuff,
+        "results_%dn_%dt_%dg_%dtest_%dq.txt",
+        NUM_NODES, NUM_THREADS, IS_GPU, TEST_TYPE, NUM_QUBITS);
+        
+    if (env.rank == 0)
+        printf("Writing results to:\n%s\n\n", fnBuff);
+
+    // write results to file
+    if (env.rank == 0) {
+        FILE *fp = fopen(fnBuff, "w");
+        if (fp) {
+            fprintf(fp,
+                "args:\n%s\nfn:\n%s\n\nresults:\nMEAN DURATION: %.10Lf\nVARIANCE: %.10Lf\n",
+                argsBuff, fnBuff, avDur, varDur);
+            fclose(fp);
+        } else
+            printf("FAILED to write to file %s\n", fnBuff);
     }
         
     // cleanup
     destroyQureg(qureg, env);
     destroyQuESTEnv(env);
     return 0;
+
 }
